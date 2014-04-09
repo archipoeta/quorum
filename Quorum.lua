b1f41e5e685cbed80770fda7cfa0bc640eb1f5ea
Chris Hart <archipoetae@gmail.com>
2014-04-09 14:24:59 -0700

    Updates

--- Quorum.lua
+++ Quorum.lua
@@ -5,20 +5,62 @@
 -- Date: 2014.04.06
 -- Repo: https://github.com/archipoeta/quorum
 
-Q = {}
-
-Q.version = "1.0.1"
-Q.formatRegex = " >\t"
-Q.characterName = GetDisplayName()
-
-Q.guilds = {
-	GetGuildName(1), GetGuildName(2), GetGuildName(3), GetGuildName(4), GetGuildName(5)
-}
-Q.guildChannels = {
-	CHAT_CHANNEL_GUILD_1, CHAT_CHANNEL_GUILD_2, CHAT_CHANNEL_GUILD_3, CHAT_CHANNEL_GUILD_4, CHAT_CHANNEL_GUILD_5
-}
-
+if ( Q == nil ) then Q = {} end
 local wm = WINDOW_MANAGER
+
+Q.version = "1.0.4"
+
+function Q.init()
+	Q.character_name = GetDisplayName()
+	Q.quora = {}
+
+	Q.guild_names = {
+		GetGuildName(1), GetGuildName(2), GetGuildName(3), GetGuildName(4), GetGuildName(5)
+	}
+
+	Q.guild_channels = {
+		CHAT_CHANNEL_GUILD_1, CHAT_CHANNEL_GUILD_2, CHAT_CHANNEL_GUILD_3, CHAT_CHANNEL_GUILD_4, CHAT_CHANNEL_GUILD_5
+	}
+
+	Q.format_regex = " ~\t"
+	Q.default_color = { 207, 220, 189 }	--ElderScrolls Default-Text (Cream/Tan)
+	Q.close_hover_color = { 255, 192, 0 }	-- Orange
+	Q.title_hover_color = { 0, 192, 255 }	-- Aqua
+	Q.tab_hover_color = { 128, 128, 240 }	-- Periwinkle
+
+	Q.tab_colors = {
+		{ 255, 221, 255 }, -- Violet
+		{ 255, 221, 255 },
+		{ 221, 255, 255 }, -- Cyan
+		{ 255, 221, 221 }, -- Red
+		{ 221, 221, 255 }, -- Indigo
+		{ 221, 221, 255 },
+		{ 221, 221, 255 }
+	}
+	
+	for i = 1, #Q.guild_names do
+		if not ( Q.guild_names[i] == "" ) then
+			local player = GetPlayerGuildMemberIndex(i)
+			local name, note, rank, stat, last = GetGuildMemberInfo( i, player )
+			Q.quora[ i ] = {
+				guild_name = Q.guild_names[i],
+				meeting_in_progress = false,
+				vote_in_progress = false,
+				votes = { ["yea"] = {}, ["nay"] = {}, ["abs"] = {}, },
+				chair = "N/A",
+				speaker = "N/A",
+				move = 0,
+				motion_body = "",
+				player_rank = rank,
+			}
+		end
+	end
+
+end
+
+function Q.init_saved_vars()
+	Q.quora = ZO_SavedVars:NewAccountWide( "Quorum_SavedVariables", 1, "quora", Q.quora )
+end
 
 function Q.AddSlashCommands()
 	SLASH_COMMANDS["/quo"] = function()
@@ -32,49 +74,37 @@ function Q.AddSlashCommands()
 	end
 end
 
-function Q.PairsByKeys(t, f)
-	local a = {}
-	for n in pairs(t) do table.insert(a, n) end
-	table.sort(a, f)
-	local i = 0
-	local iter = function ()
-		i = i + 1
-		if a[i] == nil then return nil
-		else return a[i], t[a[i]]
-		end
-	end
-	return iter
+function Q.ElementHoverOn(element, rgb)
+	element:SetColor( rgb[1]/255, rgb[2]/255, rgb[3]/255, 1 )
 end
 
-function Q.MotionBodyOK(ok, cancel, control)
-	local text = control:GetText()
-	control:SetText("")
-	control:LoseFocus()
-	control:SetHidden(true)
-	
-	ok:SetHidden(true)
-	cancel:SetHidden(true)
-	
-	CHAT_SYSTEM:AddMessage(text)
-	ZO_ChatWindowTextEntryEditBox:SetText( Q.formatRegex .. text)
-	
-	Q.ShowMainMotions()
+function Q.ElementHoverOff(element, rgb)
+	element:SetColor( rgb[1]/255, rgb[2]/255, rgb[3]/255, 1 )
 end
 
-function Q.MotionBodyCANCEL(ok, cancel, control)
-	control:LoseFocus()
-	control:SetHidden(true)
-	ok:SetHidden(true)
-	cancel:SetHidden(true)
-	
-	Q.ShowMainMotions()
+function Q.get_table_length(T)
+  local count = 0
+  for _ in pairs(T) do count = count + 1 end
+  return count
 end
 
 function Q.HandleMotion( key, value )
-	if not ( string.find( key, 'I move to' ) ) then
+	if ( string.find( key, 'Guild:\t' ) ) then
+		if ( value > 5 ) then
+			value = 1
+		elseif ( value ~= 5 ) then
+			value = value + 1
+		end
+			
+		if ( Q.guild_names[ value ] == "" ) then
+			value = 1
+		end
+				
+		Q.ShowSummary(value)
+	elseif not ( string.find( key, 'I move to' ) ) then
 		if not ( value == 0 ) then
 			CHAT_SYSTEM:AddMessage(value)
-			ZO_ChatWindowTextEntryEditBox:SetText( Q.formatRegex .. value )
+			ZO_ChatWindowTextEntryEditBox:SetText( Q.format_regex .. "|cDA77FF" .. value .. "|r" )
 		end
 	else
 		for i = 3, 10, 1 do
@@ -102,133 +132,6 @@ function Q.HandleMotion( key, value )
 	end
 end
 
-function Q.ShowActions( actions )
-	for j = 1, 10, 1 do
-		if ( wm:GetControlByName( j ) ) then
-			local control = wm:GetControlByName( j )
-			control:SetText("")
-		end
-	end
-
-	local i = 1
-	local delta_y = 1
-	
-	for key, value in Q.PairsByKeys( actions ) do
-		if not ( wm:GetControlByName( i ) ) then
-			local control = wm:CreateControl( i, Quorum, CT_LABEL )
-			control:SetParent(Quorum)
-			control:SetAnchor(TOPLEFT, Quorum, TOPLEFT, 5, 85 + delta_y)
-			control:SetResizeToFitDescendents(true)
-			control:SetMouseEnabled(true)
-			control:SetFont("ZoFontGame")
-			control:SetText(key)
-			control:SetHandler( "OnMouseDown", function() Q.HandleMotion(key, value) end )
-			control:SetHandler( "OnMouseEnter", function()
-				control:SetColor(0,1,0,1)
-			end )
-			control:SetHandler( "OnMouseExit", function()
-				control:SetColor(240,240,240,1)
-			end )
-		else
-			local control = wm:GetControlByName( i )
-			control:SetText(key)
-			control:SetHandler( "OnMouseDown", function() Q.HandleMotion(key, value) end )
-		end
-		delta_y = delta_y + 25
-		i = i + 1
-	end
-end
-
-function Q.ShowHelpMenu()
-	local motions = {
-		["\ta. Title: Quroum"]					= 0,
-		["\tb. Desc: Meeting add-on for ESO"]	= 0,
-		["\td. Author: @archpoet"]				= 0,
-		["\tc. Version: " .. Q.version ] 		= 0,
-		["\te. Help: http://imperialsenate.org/forum"]	= 0,
-	}
-
-	Q.ShowActions( motions )
-end
-
-function Q.ShowMainMotions()
-	local motions = {
-		["1. Stand to Speak/Request Floor"]	= "*SEEKS RECOGNITION OF THE CHAIR*",	
-		["2. I move to/that"]	= "MOTION TO/THAT... ",
-		["3. Second a Motion"]	= "SECONDED.",
-	}
-
-	Q.ShowActions( motions )
-end
-
-function Q.ShowPrivMotions()
-	local motions = {
-		["1. Close Meeting"]	= "MOTION TO ADJOURN.",
-		["2. Take A Break"]		= "MOTION TO RECESS.",
-		["3. Complaint"]		= "QUESTION OF PRIVILEGE.",
-		["4. Follow Agenda"]	= "CALL FOR THE ORDERS OF THE DAY.",
-	}
-
-	Q.ShowActions( motions )
-end
-
-function Q.ShowSubMotions()
-	local motions = {
-		["1. Skip Motion"]			= "MOTION TO TABLE THE QUESTION.",
-		["2. Close Debate"]			= "MOTION TO THE PREVIOUS QUESTION.",
-		["3. Limit/Extend Debate"]	= "MOTION TO LIMIT DEBATE.",
-		["3. Amend Motion"]			= "MOTION TO AMEND.",
-		["4. Substitute Motion"]	= "MOTION TO SUBSTITUTE.",
-	}
-
-	Q.ShowActions( motions )
-end
-
-function Q.ShowIncidentMotions()
-	local motions = {
-		["1. Get Information"]			= "POINT OF INFORMATION.",
-		["2. Enforce Rules"]			= "POINT OF ORDER.",
-		["3. Ignore Rules"]				= "MOTION TO SUSPEND THE RULE.",
-		["4. Appeal Chair Decision"]	= "MOTION TO APPEAL FROM THE DECISION OF THE CHAIR.",
-		["5. Withdraw Your Motion"]		= "MOTION TO WITHDRAW.",
-		["6. Objection to Motion"]		= "OBJECTION TO CONSIDERATION OF THE QUESTION.",
-		["7. Split Motion into Parts"]	= "MOTION TO DIVIDE THE QUESTION.",
-	}
-
-	Q.ShowActions( motions )
-end
-
-function Q.ShowOtherMotions()
-	local motions = {
-		["1. Reconsider skipped Motion"]	= "MOTION TO TAKE FROM THE TABLE.",
-		["2. Reconsider voted Motion"]		= "MOTION TO RECONSIDER.",
-		["3. Rescind/Revoke voted Motion"]	= "MOTION TO RESCIND.",
-		["4. Amend enacted/voted Motion"]	= "MOTION TO AMEND.",
-	}
-	
-	Q.ShowActions( motions )
-end
-
-function Q.ShowChairActions()
-	local motions = {
-		["1. Begin Meeting"]			= "CALL TO ORDER.",
-		["2. Recognize Speaker"]		= "THE CHAIR RECOGNIZES ",
-		["3. Open Debate on a Motion"]	= "IT HAS BEEN MOVED AND SECONDED, IS THERE ANY DISCUSSION?",
-	}
-	
-	Q.ShowActions( motions )
-end
-
-function Q.ShowVotingActions()
-	local motions = {
-		["1. Yes"]					= "YEA.",
-		["2. No"]					= "NAY.",
-		["3. Abstain from Voting"]	= "I ABSTAIN.",
-	}
-	
-	Q.ShowActions( motions )
-end
-
 function Q.IsIn(value, array)
 	if (#array == 0) then
 		return false
@@ -241,12 +144,314 @@ function Q.IsIn(value, array)
 	return false
 end
 
-function Q.OnMessageReceived(eventCode, messageType, fromName, text)
-	local num = Q.IsIn(messageType, Q.guildChannels)
-	if ( num and string.find( text, Q.formatRegex ) ) then
-		CHAT_SYSTEM:AddMessage( "Text matched the output of this add-on: " .. text )
-		local guild = Q.guilds[num]
+function Q.MotionBodyOK(ok, cancel, control)
+	local text = control:GetText()
+	control:SetText("")
+	control:LoseFocus()
+	control:SetHidden(true)
+	
+	ok:SetHidden(true)
+	cancel:SetHidden(true)
+	
+	CHAT_SYSTEM:AddMessage(text)
+	ZO_ChatWindowTextEntryEditBox:SetText( Q.format_regex .. text)
+	
+	Q.ShowMainMotions()
+end
+
+function Q.MotionBodyCANCEL(ok, cancel, control)
+	control:LoseFocus()
+	control:SetHidden(true)
+	ok:SetHidden(true)
+	cancel:SetHidden(true)
+	
+	Q.ShowMainMotions()
+end
+
+function Q.PairsByKeys(t, f)
+	local a = {}
+	for n in pairs(t) do table.insert(a, n) end
+	table.sort(a, f)
+	local i = 0
+	local iter = function ()
+		i = i + 1
+		if a[i] == nil then return nil
+		else return a[i], t[a[i]]
+		end
+	end
+	return iter
+end
+
+function Q.ShowActions( actions )
+	for j = 1, 10, 1 do
+		if ( wm:GetControlByName( j ) ) then
+			local control = wm:GetControlByName( j )
+			control:SetText("")
+		end
+	end
+
+	local delta_y = 1
+
+	for i = 1, #actions, 1 do
+		for j = 1, #actions[i], 1 do
+			if not ( wm:GetControlByName( i ) ) then
+				local control = wm:CreateControl( i, Quorum, CT_LABEL )
+				control:SetParent(Quorum)
+				control:SetAnchor(TOPLEFT, Quorum, TOPLEFT, 5, 43 + delta_y)
+				control:SetResizeToFitDescendents(true)
+				control:SetMouseEnabled(true)
+				control:SetFont("ZoFontGame")
+				control:SetText(actions[i][1])
+				control:SetHandler( "OnMouseDown", function() Q.HandleMotion(actions[i][1], actions[i][2]) end )
+				control:SetHandler( "OnMouseEnter", function()
+					control:SetColor(0,1,0,1)
+				end )
+				control:SetHandler( "OnMouseExit", function()
+					control:SetColor(240,240,240,1)
+				end )
+			else
+				local control = wm:GetControlByName( i )
+				control:SetText(actions[i][1])
+				control:SetHandler( "OnMouseDown", function() Q.HandleMotion(actions[i][1], actions[i][2]) end )
+			end
+		end
+		delta_y = delta_y + 25
+	end
+end
+
+function Q.ShowHelpMenu()
+	local motions = {
+		{ "\t\t Title: Quroum", 0 },
+		{ "\t\t Desc: Meeting Add-On for ESO", 0 },
+		{ "\t\t Author: @archpoet", 0 },
+		{ "\t\t Version: " .. Q.version, 0 },
+		{ "\t\t http://imperialsenate.org/forum", 0 },
+	}
+
+	Q.ShowActions( motions )
+end
+
+function Q.ShowMainMotions()
+	local motions = {
+		{ "1.\t\t I move to/that", "MOTION TO/THAT... " },
+		{ "2.\t\t Second a Motion", "SECONDED." },
+		{ "3.\t\t Yield the Floor", "I YIELD." },
+		{ "4.\t\t Withdraw Your Motion", "MOTION TO WITHDRAW." },
+		{ Q.info },
+	}
+
+	Q.ShowActions( motions )
+end
+
+function Q.ShowPrivMotions()
+	local motions = {
+		{ "1.\t\t Close Meeting", "MOTION TO ADJOURN." },
+		{ "2.\t\t Take A Break", "MOTION TO RECESS." },
+		{ "3.\t\t Complain about noise, heat, etc.", "QUESTION OF PRIVILEGE." },
+		{ "4.\t\t Follow Agenda", "CALL FOR THE ORDERS OF THE DAY." },
+	}
+
+	Q.ShowActions( motions )
+end
+
+function Q.ShowSubMotions()
+	local motions = {
+		{ "1.\t\t Skip Motion", "MOTION TO TABLE THE QUESTION." },
+		{ "2.\t\t Close Debate and Vote", "MOTION TO THE PREVIOUS QUESTION." },
+		{ "3.\t\t Limit/Extend Debate", "MOTION TO LIMIT DEBATE." },
+		{ "3.\t\t Amend Motion", "MOTION TO AMEND." },
+		{ "4.\t\t Substitute Motion", "MOTION TO SUBSTITUTE." },
+	}
+
+	Q.ShowActions( motions )
+end
+
+function Q.ShowIncidentMotions()
+	local motions = {
+		{ "1.\t\t Get Information", "POINT OF INFORMATION." },
+		{ "2.\t\t Enforce Rules", "POINT OF ORDER." },
+		{ "3.\t\t Ignore Rules", "MOTION TO SUSPEND THE RULE." },
+		{ "4.\t\t Appeal Chair Decision", "MOTION TO APPEAL FROM THE DECISION OF THE CHAIR." },
+		{ "5.\t\t Objection to Motion", "OBJECTION TO CONSIDERATION OF THE QUESTION." },
+		{ "6.\t\t Split Motion into Parts", "MOTION TO DIVIDE THE QUESTION." },
+	}
+
+	Q.ShowActions( motions )
+end
+
+function Q.ShowOtherMotions()
+	local motions = {
+		{ "1.\t\t Reconsider skipped Motion", "MOTION TO TAKE FROM THE TABLE." },
+		{ "2.\t\t Reconsider voted Motion", "MOTION TO RECONSIDER." },
+		{ "3.\t\t Rescind/Revoke voted Motion", "MOTION TO RESCIND." },
+		{ "4.\t\t Amend carried/voted Motion", "MOTION TO AMEND." },
+	}
+	
+	Q.ShowActions( motions )
+end
+
+function Q.ShowChairActions()
+	local motions = {
+		{ "1.\t\t Begin Meeting", "CALL TO ORDER." },
+		{ "2.\t\t Recognize Speaker", "THE CHAIR RECOGNIZES " },
+		{ "3.\t\t Recess Meeting", "MEETING IS IN RECESS." },
+		{ "4.\t\t End Meeting", "MEETING IS ADJOURNED." },
+	}
+	
+	Q.ShowActions( motions )
+end
+
+function Q.ShowChairAnswers()
+	local motions = {
+		{ "1.\t\t Enforce Order", "POINT OF ORDER." },
+		{ "2.\t\t Point Accepted", "YOUR POINT IS WELL TAKEN." },
+		{ "3.\t\t Point Denied", "YOUR POINT IS NOT WELL TAKEN." },
+		{ "4.\t\t Motion Succeeds", "MOTION CARRIED." },
+		{ "5.\t\t Motion Fails", "MOTION DENIED." },		
+		{ "6.\t\t Sustain Objection", "SUSTAINED." },
+		{ "7.\t\t Overrule Objection", "OVERRULED." },
+	}
+	
+	Q.ShowActions( motions )
+end
+
+function Q.ShowChairQuestions()
+	local motions = {
+		{ "1.\t\t Any Seconds?", "MOTION ON THE FLOOR, DO I HEAR A SECOND?" },
+		{ "2.\t\t Open Debate on a Motion", "IT HAS BEEN MOVED AND SECONDED, IS THERE ANY DISCUSSION?" },
+		{ "3.\t\t Ask for Point", "WHAT IS YOUR POINT?" },
+		{ "4.\t\t Two-thirds to Close Debate", "THE QUESTION HAS BEEN CALLED. SHALL IT BE PUT?" },
+		{ "5.\t\t Open Vote on a Motion", "THE QUESTION HAS BEEN PUT. ALL IN FAVOR, OPPOSED?" },
+		{ "6.\t\t Ask speaker to yield", "WILL YOU YIELD THE FLOOR?" },
+	}
+	
+	Q.ShowActions( motions )
+end
+
+function Q.ShowVotingActions()
+	local motions = {
+		{ "1.\t\t Yes", "YEA." },
+		{ "2.\t\t No", "NAY." },
+		{ "3.\t\t Abstain from Voting", "I ABSTAIN." },
+	}
+	
+	Q.ShowActions( motions )
+end
+
+function Q.ShowSummary( guild )
+	for j = 1, 10, 1 do
+		if ( wm:GetControlByName( j ) ) then
+			local control = wm:GetControlByName( j )
+			control:SetText("")
+		end
+	end
+
+	local meetings = {}
+
+	if ( guild == nil ) or ( guild == 0 ) then
+		if not ( Q.active_guild == nil ) then
+			guild = Q.active_guild
+		else
+			guild = 1
+		end
+	end
+
+	Q.active_guild = guild
+
+	local meeting_count = 0
+	local v = Q.quora[guild]
+
+	if ( v ~= nil and type(v) == "table" ) then
+		--if ( v.meeting_in_progress == true ) then
+			--meeting_count = meeting_count + 1
+			Q.ShowActions( {
+				{ "Guild:\t\t " .. tostring(v.guild_name) .. "\t\t>>", guild },
+				{ "Meeting Now:\t\t " .. tostring(v.meeting_in_progress), 0 },
+				{ "Chair:\t\t " .. tostring(v.chair), 0 },
+				{ "Floor:\t\t " .. tostring(v.speaker), 0 },
+				{ "Move:\t\t "  .. tostring(v.move), 0 },
+				{ "Question:\t\t " .. tostring(v.motion_body), 0 },
+				{ "Voting Now:\t\t " .. tostring(v.vote_in_progress), 0 },
+				{ "Votes:\t\t " .. tostring( Q.get_table_length(v.votes.yea) ) .. " / " .. tostring( Q.get_table_length(v.votes.nay) ) .. " / " .. tostring( Q.get_table_length(v.votes.abs) ), 0 },
+			} )
+		--end
+
+		if ( v.player_rank > 2 ) or ( v.player_rank == nil ) then
+			Section7:SetHidden(true)
+			Section7.hidden = true
+			Section8:SetHidden(true)
+			Section8.hidden = true
+			Section9:SetHidden(true)
+			Section9.hidden = true			
+		elseif ( v.player_rank < 2 ) then
+			Section7:SetHidden(false)
+			Section7.hidden = false
+			Section8:SetHidden(false)
+			Section8.hidden = false
+			Section9:SetHidden(false)
+			Section9.hidden = false			
+		end
+
+		if ( v.meeting_in_progress == false ) then
+			--Q.ShowActions( { { "No Meeting currently in session.", 0 } } )
+			Notify:SetText( "No Meeting." )
+		end
+	end
+end
+
+-- parse lines of guild chat to watch for votes,motions,etc.
+function Q.OnMessageReceived(event_code, message_type, from_name, text)
+	local guild_id = Q.IsIn(message_type, Q.guild_channels)
+	if ( guild_id and string.find( text, Q.format_regex ) ) then
+		--CHAT_SYSTEM:AddMessage( "Text matched the output of this add-on: " .. text )
+		local message = text:gsub( Q.format_regex, "" )
 		
+		if ( string.find( message, "CALL TO ORDER." ) ) then
+			Quorum:SetHidden(false)
+			Quorum.hidden = false		
+
+			Q.quora[guild_id].meeting_in_progress = true
+			Q.quora[guild_id].chair = from_name
+			Q.quora[guild_id].speaker = from_name
+			Q.quora[guild_id].motion_body = message
+
+			Notify:SetText( guild_id .. ": " .. message )
+		--elseif ( message == "" ) then
+		
+		elseif ( string.find( message, "YEA." ) ) then
+			if ( Q.quora[guild_id].votes.yea[ Q.character_name ] == nil ) then
+				Q.quora[guild_id].votes.yea[ Q.character_name ] = 1
+				Q.quora[guild_id].votes.nay[ Q.character_name ] = nil
+				Q.quora[guild_id].votes.abs[ Q.character_name ] = nil
+			end
+		elseif ( string.find( message, "NAY." ) ) then
+			if ( Q.quora[guild_id].votes.nay[ Q.character_name ] == nil ) then
+				Q.quora[guild_id].votes.nay[ Q.character_name ] = 1
+				Q.quora[guild_id].votes.yea[ Q.character_name ] = nil
+				Q.quora[guild_id].votes.abs[ Q.character_name ] = nil				
+			end		
+		elseif ( string.find( message, "I ABSTAIN." ) ) then
+			if ( Q.quora[guild_id].votes.abs[ Q.character_name ] == nil ) then
+				Q.quora[guild_id].votes.abs[ Q.character_name ] = 1
+				Q.quora[guild_id].votes.yea[ Q.character_name ] = nil
+				Q.quora[guild_id].votes.nay[ Q.character_name ] = nil
+			end		
+		elseif ( string.find( message, "MEETING ADJOURNED." ) ) then
+			Q.quora[guild_id].meeting_in_progress = false
+			Q.quora[guild_id].vote_in_progress = false
+			Q.quora[guild_id].chair = "N/A"
+			Q.quora[guild_id].speaker = "N/A"
+			Q.quora[guild_id].move = 0
+			Q.quora[guild_id].motion_body = message
+		else
+			Q.quora[guild_id].motion_body = message
+		end
+
+		if ( Q.quora[guild_id].speaker == Q.character_name ) then
+			Notify:SetText( guild_id .. ": " .. "|c66FF66You have the floor!|r" )
+		else
+			Notify:SetText( guild_id .. ": " .. "|cFF6666You do not have the floor.|r" )
+		end
 --		SpamFilter.Debug("Validating message from "..fromName.." at "..tostring(GetTimeStamp()))
 --		local ruleBroken = RuleBroken(fromName, text)
 --		if (not IsIgnored(fromName) and ruleBroken ~= nil) then
@@ -266,9 +471,10 @@ function Q.OnMessageReceived(eventCode, messageType, fromName, text)
 	end
 end
 
--- TODO:
--- Add EVENT_MANAGER for reading chat here,
--- parse lines of guild chat to watch for votes,motions,etc.
+--
+-- MAIN
+--
 
+Q.init()
 Q.AddSlashCommands()
 EVENT_MANAGER:RegisterForEvent("Quorum", EVENT_CHAT_MESSAGE_CHANNEL, Q.OnMessageReceived)
